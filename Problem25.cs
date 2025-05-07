626ef1021e960f346aea69cfb4f3b779f19f1902
daftfrik <daft.frik@gmail.com>
2025-05-07 04:33:51 +0100

    Fixes

--- Problem25.cs
+++ Problem25.cs
@@ -1,35 +1,34 @@
 using System;
 using System.Numerics;
 
-namespace Project_Euler {
-    public class Problem25 : Problem{
-        public override void Solve() {
-            Print(FibonacciNDigits(1000));
-        }
+namespace Project_Euler;
+public class Problem25 : Problem{
+    public override void Solve() {
+        Print(FibonacciNDigits(1000));
+    }
 
-        private int FibonacciNDigits(int n) {
-            int index = 1, length = 0;
-            BigInteger fib1 = 0;
-            BigInteger fib2 = 1;
+    private int FibonacciNDigits(int n) {
+        int index = 1, length = 0;
+        BigInteger fib1 = 0;
+        BigInteger fib2 = 1;
 
-            while(length < n){
-                BigInteger sum = fib1 + fib2;
-                fib1 = fib2;
-                fib2 = sum;
-                length = GetDigitCount(sum);
-                index++;
-            }
-            return index;
+        while(length < n){
+            BigInteger sum = fib1 + fib2;
+            fib1 = fib2;
+            fib2 = sum;
+            length = GetDigitCount(sum);
+            index++;
         }
+        return index;
+    }
 
-        private int GetDigitCount(BigInteger number) {
-            double factor = Math.Log(2) / Math.Log(10);
-            //int digitCount = (int) (factor * number. + 1);
-            int digitCount = (int)Math.Floor(BigInteger.Log10(number) + 1);
-            if (BigInteger.Pow(10, digitCount - 1) == number) {
-                return digitCount - 1;
-            }
-            return digitCount;
+    private int GetDigitCount(BigInteger number) {
+        double factor = Math.Log(2) / Math.Log(10);
+        //int digitCount = (int) (factor * number. + 1);
+        int digitCount = (int)Math.Floor(BigInteger.Log10(number) + 1);
+        if (BigInteger.Pow(10, digitCount - 1) == number) {
+            return digitCount - 1;
         }
+        return digitCount;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Collections;
+using System.Diagnostics;
+using System.Reflection;
 
-Console.WriteLine("Hello, World!");
+namespace Project_Euler;
+
+internal class Program {
+    private ArrayList _solvedProblems = null!;
+    public static void Main(string[] args) {
+        Program prog = new Program();
+        Console.WriteLine("Project Euler Solver");
+        int chosenProblem = prog.GetInput();
+        if (chosenProblem < 0) prog.SolveAll();
+        else prog.Solve(chosenProblem);
+    }
+    
+    private int GetInput() {
+        _solvedProblems ??= GetSolvedProblems();
+        Console.WriteLine("Enter 'a' to solve all problems.");
+        Console.Write("Enter Problem to solve (1 - {0}): ", _solvedProblems.Count);
+        string? input = Console.ReadLine();
+        bool success = int.TryParse(input, out int result);
+        return success && result > 0 ? result : -1;
+    }
+
+    private ArrayList GetSolvedProblems() {
+        ArrayList solvedProblems = new ArrayList();
+        Type[]? allTypes = Assembly.GetAssembly(typeof(Problem))?.GetTypes();
+        foreach (Type type in allTypes!) 
+            if (type.IsSubclassOf(typeof(Problem)))
+                solvedProblems.Add((Problem)Activator.CreateInstance(type)!);
+        return solvedProblems;
+    }
+
+    private void Solve(int n) {
+        Problem problem = (Problem)_solvedProblems[n-1]!;
+        Stopwatch watch = Stopwatch.StartNew();
+        problem.Solve();
+        watch.Stop();
+        Console.WriteLine("Solved in {0} ms", watch.ElapsedMilliseconds);
+    }
+
+    private void SolveAll() {
+        Stopwatch watch = Stopwatch.StartNew();
+        for(int i = 1; i <= _solvedProblems.Count; i++) Solve(i);
+        watch.Stop();
+        Console.WriteLine("Solved all problems in {0} ms", watch.ElapsedMilliseconds);
+    }
+}
